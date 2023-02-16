4e3bfbff5b71d84aaadcd0b914268bdf87ad622a
alvinvercetti <122325814+alvinvercetti@users.noreply.github.com>
2023-02-15 20:18:52 -0500

    Update rock-paper-scissors.py

--- not done/rock-paper-scissors.py
+++ not done/rock-paper-scissors.py
@@ -45,3 +45,33 @@ def qc(): #"qc" is short for "quit or continue". this will be called at the end 
         print("goodbye")
     else:
         qc()
+        
+NEW SCRIPT (semi comlete) down below
+import random
+while True:
+    user_action = input("Enter a choice (rock, paper, scissors): ")
+    possible_actions = ["rock", "paper", "scissors", "Rock", "Paper", "Scissors"]
+    computer_action = random.choice(possible_actions)
+    print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+    if random: print("you have to pick between rock, paper and scissors, not something random... NERD")
+    if user_action == computer_action:
+        print(f"Both players selected {user_action}. It's a tie... NERD!")
+    elif user_action == "rock":
+        if computer_action  == "scissors":
+            print("rock beats scissors, you win.")
+        else:
+            print("paper covers rock, you lose... LOSER")
+    elif user_action == "paper":
+        if computer_action == "rock":
+            print("paper covers rock! you win.")
+        else:
+            print("scissors cuts paper, you lose... LOSER NERD")
+    elif user_action == "scissors":
+        if computer_action == "paper":
+            print("scissors cuts paper, you win.")
+        else:
+            print("rock smashes scissors, you lose... IMBECIL")
+
+    play_again = input("do you wish to play again? (y/n): ")
+    if play_again.lower() != "y":
+        break
